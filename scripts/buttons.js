f1cb71b82b15221717d0f660dbcf08c1ecd18668
Konstantin <turbo.foxy01@gmail.com>
2024-03-10 17:34:18 +0100

    made the calculator more resposive

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-    $('[id^="cal"]').click(function() {
-        var id_atr = $(this).attr('id')
-        $("#berekening").val($("#berekening").val()+id_atr[id_atr.length-1])
+    $('[id^="cal-"]').click(function() {
+        var special_opps = ["cal-=","cal-AC"];
+        var id_atr = $(this).attr('id');
+        
+        if (!special_opps.includes(id_atr)){
+        $("#berekening").val($("#berekening").val()+id_atr[id_atr.length-1]);
+        }
+        else{
+            switch (id_atr){
+                case "cal-=":
+                    var eval_to = eval($("#berekening").val())
+                    $("#berekening").val(eval_to);
+                    break;
+                case "cal-AC":
+                    
+                    $("#berekening").val('');
+                    break;
+            }
+
+            
+        }
     });
 });
